4937ddb0eba5d0e4b27a166b2faebe8104b3823b
Christopher Johnson <christopherjohnson@Christophers-Mac-mini.local>
2021-08-01 15:52:29 -0400

    test

--- src/App.js
+++ src/App.js
@@ -3,29 +3,11 @@ import './App.css';
 import React, {useState, useEffect} from 'react'
 
 function App() {
-  const apiKey='https://cdn.contentful.com/spaces/lqgw03zjuy3r/environments/master/entries?access_token=IxLPmtOyDSuJ7FVVftr9sAg5Qf0RdJcxogZ9nDZv1VE&content_type=book'
-  const [books, setBooks] = useState({})
-  const pickBook = async() =>{
-    const response = await fetch(apiKey)
-    const data = await response.json()
-    const fixData = data.items.map( (book, index) => {
-          return {
-            title: book.fields.title,
-            author: book.fields.author,
-            description : book.fields.description,
-            ranking: book.fields.ranking,
-            cover:  data.includes.Asset[index].fields.file.url
 
-          }
-    })
-    console.log(fixData)
-    setBooks(fixData)
-    }
+   
   
   
-  useEffect(() => {
-    pickBook()
-}, [])
+
 
 
   
